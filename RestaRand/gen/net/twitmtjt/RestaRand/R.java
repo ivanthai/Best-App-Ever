e3967270af14b1f5293b0025ddf7d2437363ab72
Jennifer To <jenardto@gmail.com>
2011-09-11 04:21:21 -0700

    we win yay :)

--- RestaRand/gen/net/twitmtjt/RestaRand/R.java
+++ RestaRand/gen/net/twitmtjt/RestaRand/R.java
@@ -10,14 +10,30 @@ package net.twitmtjt.RestaRand;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background=0x7f040000;
+        public static final int menu=0x7f040001;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int addBtn=0x7f060001;
+        public static final int go_button=0x7f060002;
+        public static final int restaurantName=0x7f060000;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int activity2=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int add=0x7f050007;
+        public static final int app_name=0x7f050000;
+        public static final int go_label=0x7f050003;
+        public static final int main_title1=0x7f050001;
+        public static final int main_title2=0x7f050002;
+        public static final int menu_label=0x7f050004;
+        public static final int result_text=0x7f050006;
+        public static final int result_title=0x7f050005;
     }
 }
--- RestaRand/src/net/twitmtjt/RestaRand/RestaRandActivity.java
+++ RestaRand/src/net/twitmtjt/RestaRand/RestaRandActivity.java
@@ -1,13 +1,120 @@
 package net.twitmtjt.RestaRand;
 
-import android.app.Activity;
+import android.app.*;
 import android.os.Bundle;
+
+import android.widget.*;
+import android.view.*;
+import android.content.*;
+import java.io.*;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class RestaRandActivity extends Activity {
     /** Called when the activity is first created. */
-    @Override
+    
+	public static Scanner start;
+	ArrayList<String> masterArray = new ArrayList<String>();
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        try {
+        	saveArray();
+        	loadArray();
+        }
+        catch (IOException e) {
+        	Toast.makeText(getBaseContext(), "UH OH", Toast.LENGTH_LONG).show();
+        }
+        
+        //parse txt file of names and push into masterArray
+        //masterArray = 
+        
+		Button goBtn = (Button) findViewById(R.id.go_button);
+		goBtn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Random randomGenerator = new Random();
+				int index = randomGenerator.nextInt(masterArray.size());
+				Toast.makeText(getBaseContext(), masterArray.get(index).toString(),
+						Toast.LENGTH_SHORT).show();
+	        }
+        });	
+    }
+	
+	private void saveArray() throws IOException{
+        try {
+        FileOutputStream fOut = openFileOutput("restarand.txt", MODE_WORLD_READABLE);
+        OutputStreamWriter osw = new OutputStreamWriter(fOut);
+        osw.write("Gypsy's\nThai Basil\nCheeseboard\n");
+			osw.flush();
+            osw.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	private void loadArray() throws IOException {
+		try {
+			FileInputStream fIn = openFileInput("restarand.txt");
+			InputStreamReader isr = new InputStreamReader(fIn);
+			char[] inputBuffer = new char[100];
+			String s = "";
+
+			int charRead;
+			while ((charRead = isr.read(inputBuffer)) > 0) {
+				String readString = String
+						.copyValueOf(inputBuffer, 0, charRead);
+				s += readString;
+				inputBuffer = new char[100];
+			}
+			String[] array = s.split("\n");
+			for(int i = 0; i < array.length; i++) {
+				masterArray.add(array[i].toString());
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		CreateMenu(menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return MenuChoice(item);
+	}
+	
+	protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+		//masterArray = data.getStringArrayListExtra("arrayListExtra");
+		masterArray = data.getStringArrayListExtra("arrayListExtra");
+	}
+	
+	private void CreateMenu(Menu menu) {
+		MenuItem mnu1 = menu.add(0, 0, 0, "Add Restaurant Names"); {
+			mnu1.setAlphabeticShortcut('a');
+			mnu1.setIcon(R.drawable.icon);
+		}
+	}
+    
+    private boolean MenuChoice(MenuItem item) {
+    	switch (item.getItemId()) {
+    	case 0:
+    	    Bundle b = new Bundle();
+    	    b.putStringArrayList("arrayListExtra", masterArray);
+    	    
+    		Intent mintent = new Intent(this, Activity2.class);
+    		mintent.putExtras(b);
+    		startActivityForResult(mintent, 1);
+    		return true;
+    	}
+    	return false;
     }
 }
